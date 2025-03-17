bfc8964674f38b05aaa6d94376262ea4d41abae0
vercris <alvaroh1399@gmail.com>
2025-03-17 11:50:20 -0500

    Update Project with Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ConvertCardComponent } from './ConvertCard/ConvertCard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ConvertCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
